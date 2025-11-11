878c7b1ff7e8bf7d49f353deadf898a83ad4b2d8
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-11-11 09:09:30 +0545

    Updated

--- lib/copywith.dart
+++ lib/copywith.dart
@@ -6,7 +6,7 @@ class Person {
 
   Person({required this.name, required this.age});
 
-  Person CopyWith({String? name, int? age}) {
+  Person copyWith({String? name, int? age}) {
     return Person(name: name ?? this.name, age: age ?? this.age);
   }
 }
@@ -14,7 +14,7 @@ class Person {
 void main() {
   Person p1 = Person(name: "Diwas", age: 20);
   print(p1.name);
-  p1 = p1.CopyWith(name: "Ram");
+  p1 = p1.copyWith(name: "Ram");
   print(p1.name);
   print(p1.age);
 }
